b301ee19747ce3c65c75115b083998429e3bb8ce
Tom Prince <tom.prince@ualberta.net>
2013-06-12 21:07:56 -0600

    Add a function to get the svn branch of the current git commit.

--- twisted_tools/test/test_git.py
+++ twisted_tools/test/test_git.py
@@ -43,3 +43,69 @@ class EnsureGitRepository(TestCase):
 
         d.addCallback(lambda _: git.ensureGitRepository(gitRepo.path))
         return d
+
+
+class SVNLogTests(TestCase):
+    """
+    Tests for L{git._getSVNPathFromGitLog}.
+    """
+
+    def test_succesfulParse(self):
+        """
+        When passed a commit message with a proper C{git-svn-id} line,
+        L{git._getSVNPathFromGitLog} returns the branch and revision of
+        from the tag.
+        """
+        message = """\
+Merge tcpclient-endpoint-bindaddress-6465: Parse the bindAddress keyword in TCP related client endpoint strings
+
+Author: rwall
+Reviewer: therve,exarkun
+Fixes: #6465
+Refs: #6562
+
+Parse the bindAddress keyword argument in TCP related client endpoint
+strings. The value is treated as a host string and is coerced to a
+(host, port) tuple. The port is set to 0 to bind the client to any
+ephemeral port.
+
+git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/trunk@38706 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb
+"""
+        result = git._getSVNPathFromGitLog(message)
+        self.assertEqual(result, ('/trunk', '38706'))
+
+
+    def test_missingTag(self):
+        """
+        When L{git._getSVNPathFromGitLog} is given a message whose last line
+        contains three words but without the C{git-svn-id} tag, it raises
+        L{git.NotASVNRevision}
+        """
+        message = "three word line"
+        self.assertRaises(git.NotASVNRevision, git._getSVNPathFromGitLog, message)
+
+
+    def test_notThreeWords(self):
+        """
+        When L{git._getSVNPathFromGitLog} is given a message whose last line
+        doesn't contain three words, it raises L{git.NotASVNRevision}
+        """
+        message = """\
+Parse the bindAddress keyword argument in TCP related client endpoint
+strings. The value is treated as a host string and is coerced to a
+(host, port) tuple. The port is set to 0 to bind the client to any
+ephemeral port.
+"""
+        self.assertRaises(git.NotASVNRevision, git._getSVNPathFromGitLog, message)
+
+
+    def test_wrongSVNPath(self):
+        """
+        When L{git._getSVNPathFromGitLog} is given a message whose last line
+        contains a proper C{git-svn-id} line, but which isn't the canonical
+        twisted repository, it raises L{git.NotASVNRevision}
+        """
+        message = """\
+git-svn-id: svn+ssh://svn.twistedmatrix.com/svn/Twisted/trunk@38706 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb
+"""
+        self.assertRaises(git.NotASVNRevision, git._getSVNPathFromGitLog, message)
