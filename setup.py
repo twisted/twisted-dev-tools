72e9f9bee1786611dd9e6cd1ba7006a2c0bf4c2e
Tom Prince <tom.prince@ualberta.net>
2013-06-14 16:09:29 -0600

    Version bump.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='twisted-dev-tools',
-    version='0.0.1',
+    version='0.0.2',
     url='https://github.com/twisted/twisted-dev-tools',
     description="Tools for twisted development",
     license='MIT',
